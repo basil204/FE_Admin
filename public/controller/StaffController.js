f10de31148ba5be8a0a2ae194130f9e8bf8cc1f0
basil204 <96483932+basil204@users.noreply.github.com>
2024-11-25 11:36:35 +0700

    update log

--- public/controller/StaffController.js
+++ public/controller/StaffController.js
@@ -1,6 +1,6 @@
 app.controller("StaffController", function ($scope, $http, $location) {
   const token = localStorage.getItem("authToken");
-  const API_BASE_URL = "http://localhost:1234/api";
+  const API_BASE_URL = "http://160.30.21.47:1234/api";
 
   // Authorization Header
   const config = {
